ec5c8aa40e9768a8fb030e91a83a0485757765ad
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-19 22:45:18 -0300

    commit

--- appiadas/lib/main.dart
+++ appiadas/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +17,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Piada{
+  final String pergunta;
+  final String resposta;
+
+  Piada({required this.pergunta, required this.resposta});
+
+}
+
 //stf
 
 class AppiadasScreen extends StatefulWidget {
@@ -25,6 +35,30 @@ class AppiadasScreen extends StatefulWidget {
 }
 
 class _AppiadasScreenState extends State<AppiadasScreen> {
+
+  final List<Piada> _piadas = [
+    Piada(
+      pergunta: "Por que o desenvolvedor levou o computador para o médico?",
+      resposta: "Porque ele esatava com vírus?",
+    ),
+    Piada(
+      pergunta: "Por que o livro de matemática tava triste?",
+      resposta: "Porque ele tinha vários problemas",
+    ),
+  ];
+
+  Piada _piadaSelecionada = Piada(
+    pergunta: "Aqui terá uma piada",
+    resposta: "Clique no botão abaixo",
+  );
+
+  void _trocarPiada(){
+    final indice = Random().nextInt(_piadas.length);
+    setState(() {
+    _piadaSelecionada = _piadas[indice];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,19 +73,31 @@ class _AppiadasScreenState extends State<AppiadasScreen> {
         ),
 
         //ch
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Aqui terá uma piada",
-              style: TextStyle(
-                fontSize: 24,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.bold,
-                color: Colors.orangeAccent,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "${_piadaSelecionada.pergunta}\n\n${_piadaSelecionada.resposta}",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orangeAccent,
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 24),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(20)
+                  ),
+                  onPressed: _trocarPiada, child: Text("Nova piada")),
+              )
+            ],
+          ),
         ),
       ),
     );
